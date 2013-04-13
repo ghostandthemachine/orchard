d44c022ba99811060d72febaa7d9ed471b17f082
ghostandthemachine <jonrose08@gmail.com>
2013-04-13 10:34:43 -0700

    fixing project

--- src/think/project.cljs
+++ src/think/project.cljs
@@ -25,7 +25,7 @@
     {:id 5
      :content "msfmnrsyjrjt;kjgnw;gn ngkjwngwk;jgnw"}})
 
-(def projects
+(def test-projects
   {1 {:title "Something Interesting"
       :meta
        {:author "Joe Smith"
@@ -42,7 +42,6 @@
         :team "dev"}
         :pages pages}})
 
-
 (defview new-project-form
   []
   [:form
@@ -50,24 +49,21 @@
    [:input#new-project-btn {:type "submit" :value "+"}]]
   :submit #(log (str "New Project: " (.-value (sel1 :#new-project-input)))))
 
-
 (defview project-list-item
   [p]
   [:li (:title p)]
   :click #(log (str "Select project " (:title p))))
-
 
 (defview project-menu
   [projects]
   [:div
    [:h3 "Projects"]
    (new-project-form)
-   [:ul (map project-list-item (vals projects))]])
-
+   [:ul (map project-list-item (vals test-projects))]])
 
 (defn init
   []
-  (dom/append! (sel1 :body) (project-menu (vals projects))))
+  (dom/append! (sel1 :body) (project-menu (vals test-projects))))
 
 ;  (on-realised (model/all-projects)
 ;               #(.appendChild (sel1 :body)
