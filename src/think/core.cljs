69e557cb10ddcfe9ca870fedef4e8a126f5e9bd8
Jeff Rose <rosejn@gmail.com>
2013-06-16 23:40:11 +0100

    loading resources in cljs

---
+++ src/think/core.cljs
@@ -0,0 +1,78 @@
+(ns think.core
+  (:use-macros [dommy.macros :only [sel sel1 node]])
+  (:require [dommy.core :as dom]))
+
+
+(def STYLESHEETS
+  ["css/codemirror.css",
+   "bootstrap/css/bootstrap.min.css",
+   "font-awesome/css/font-awesome.min.css",
+   "css/jquery-ui.css",
+   "css/thinker.css"])
+
+
+(def SCRIPTS
+  ["js/markdown_parser.js",
+   "js/d3.v3.min.js",
+   "js/codemirror.js",
+   "js/cm/clojure.js",
+   "js/cm/markdown.js",
+   "js/cm/gfm.js",
+   "js/jquery-1.9.0.js",
+   "js/jquery.sortable.min.js",
+   "js/jquery-ui.js",
+   "js/throttle.js",
+   ;"js/pdf/compatibility.js",
+   ;"js/pdf/l10n.js",
+   "js/pdf.js",
+   ;"js/pdf/debugger.js",
+   ;"js/pdf/viewer.js",
+   "bootstrap/js/bootstrap.min.js",
+   "js/thinker.js"])
+
+
+(def $body (sel1 :body))
+(def $head (sel1 :head))
+
+(defn load-script
+  [path]
+  (let [fpath  (str "file://" path)]
+    (dom/append! $body
+                 [:script {:type "text/javascript"
+                           :async false
+                           :src fpath}])))
+
+
+(defn load-css
+  [path]
+  (let [fpath  (str "file://" path)]
+    (dom/append! $head
+                 [:link {:type "text/css"
+                         :rel "stylesheet"
+                         :href fpath}])))
+
+
+
+(defn load-stylesheets
+  []
+  (doall (map load-css STYLESHEETS)))
+
+
+(defn load-javascripts
+  []
+  (doall (map load-script SCRIPTS)))
+
+
+
+(defn start-app
+  []
+  (try
+    (load-stylesheets)
+    (load-javascripts)
+    ;(js/process.on handle-errors)
+    ;(aset window "onerror" handle-errors)
+    (catch js/Object e
+      (log (str "Loading Error: " e))
+      (log-obj e))))
+
+;(start-app)
