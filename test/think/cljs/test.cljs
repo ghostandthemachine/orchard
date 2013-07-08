2a1246dc1d5ebd6586cd2acfba73b752c9866654
ghostandthemachine <jonrose08@gmail.com>
2013-07-07 17:06:56 -0700

    add test

---
+++ test/think/cljs/test.cljs
@@ -0,0 +1,11 @@
+(ns think.cljs.test
+	(:require [cemerick.cljs.test :as t]))
+
+
+(defn run-tests
+	[]
+	; (t/test-ns 'think.cljs.test.model.document)
+	; (t/test-ns 'think.cljs.test.model.document)
+	; (t/test-ns 'think.cljs.test.model.document)
+	; (t/test-ns 'think.cljs.test.model.document)
+	(t/test-ns 'think.cljs.test.model))
