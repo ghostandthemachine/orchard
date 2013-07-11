ca6217b67da3359e5abf8634fafce288fd0a6c8c
ghostandthemachine <jonrose08@gmail.com>
2013-07-10 17:58:51 -0700

    add a couple tests for defonce

--- src/think/couchdb.cljs
+++ src/think/couchdb.cljs
@@ -7,14 +7,6 @@
             [think.util.os :as os]
             [think.util.log :refer (log log-obj log-err)]))
 
-
-(defonce ::foo "bar")
-;; this should not work and foo should be set to "bar" again
-(defonce ::foo "woz")
-
-
-(mac ::foo 1)
-
 (def db* (atom nil))
 
 (defn log-handler
---
+++ test/think/cljs/test/defonce.cljs
@@ -0,0 +1,40 @@
+(ns think.cljs.test.defonce
+	(:require-macros [think.macros :refer [defonce]]
+									 [cemerick.cljs.test :refer [is deftest]])
+	(:require [think.util.log :refer [log log-obj]]
+				    [think.cljs.test :refer [test-ns]]))
+
+
+(defonce ::foo "bar")
+
+(defonce ::foo "woz")
+
+(deftest test-defonce
+	(is
+		(= foo "bar")))
+
+
+
+;; test that defonce allows for new defs in other namespaces
+(ns think.cljs.test.defonce2
+	(:require-macros [think.macros :refer [defonce]]
+									 [cemerick.cljs.test :refer [is deftest]])
+	(:require [think.util.log :refer [log log-obj]]
+				    [think.cljs.test :refer [test-ns]]))
+
+(defonce ::foo "woz")
+
+(deftest test-defonce
+	(is
+		(= foo "woz"))
+	(is
+		(not= foo "bar")))
+
+
+
+
+(comment
+
+	;; run tests by loading code into repl then running
+	(test-ns 'think.cljs.test.defonce)
+	)
